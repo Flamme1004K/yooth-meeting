600a9e99dd52d9a8a9cd0a6ff8d314d25c25c227
flamme <flamme1004@gmail.com>
2022-07-26 22:42:15 +0900

    feat: 프로젝트 구조를 만들어라

---
+++ src/main/java/com/youth/meeting/YoothMeetingApplication.java
@@ -0,0 +1,13 @@
+package com.youth.meeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoothMeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoothMeetingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youth/meeting/YoothMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youth.meeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoothMeetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
