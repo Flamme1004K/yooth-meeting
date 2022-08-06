3adae6fe4978b3e2b66a0d97cbc61cc9a24c6097
flamme <flamme1004@gmail.com>
2022-08-06 23:01:26 +0900

    refactor : 주최자, 참여자에 대한 생성자를 변경하라

--- src/test/java/com/youth/meeting/domain/member/MemberTest.java
+++ src/test/java/com/youth/meeting/domain/member/MemberTest.java
@@ -10,13 +10,13 @@ class MemberTest {
 
     @Test
     void 주최자를_생성한다() {
-        Member member = new Member("aaaa", "adsf!@34", "adsf!@34", LocalDate.of(2022, 03, 03), "name", "남", "채식주의자");
+        Member member = Member.JoinOrganizer("aaaa", "adsf!@34", "adsf!@34", LocalDate.of(2022, 03, 03), "name", "남", "채식주의자");
         assertThat(member.getMemberStatuses()).containsExactly(MemberStatus.ORGANIZER);
     }
 
     @Test
     void 참여자를_생성한다() {
-        Member member = new Member("aaaa", "adsf!@34", "adsf!@34", LocalDate.of(2022, 03, 03), "name", "남", "adsf", "asdf");
+        Member member = Member.JoinParticipant("aaaa", "adsf!@34", "adsf!@34", LocalDate.of(2022, 03, 03), "name", "남", "adsf", "asdf");
         assertThat(member.getMemberStatuses()).containsExactly(MemberStatus.PARTICIPANT);
     }
 
