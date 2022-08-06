220a1ec0ecea7138a4505b1b19c6241e89c2ee95
flamme <flamme1004@gmail.com>
2022-08-06 22:49:19 +0900

    test: 주최자 생성에 대한 단위 테스트를 작성하라

---
+++ src/test/java/com/youth/meeting/domain/member/MemberTest.java
@@ -0,0 +1,17 @@
+package com.youth.meeting.domain.member;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberTest {
+
+    @Test
+    void 주최자를_생성한다() {
+        Member member = new Member("aaaa", "adsf!@34", "adsf!@34", LocalDate.of(2022, 03, 03), "name", "남", "채식주의자");
+        assertThat(member.getMemberStatuses()).containsExactly(MemberStatus.ORGANIZER);
+    }
+
+}
