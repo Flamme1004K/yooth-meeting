409538c8c2be5fb3c9e95e3eb7c2d52ad34397ce
flamme <flamme1004@gmail.com>
2022-08-06 23:20:59 +0900

    test: 참여자의 정보를 수정하는 단위 테스트를 작성하라

--- src/test/java/com/youth/meeting/domain/member/MemberTest.java
+++ src/test/java/com/youth/meeting/domain/member/MemberTest.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class MemberTest {
 
@@ -36,7 +37,7 @@ class MemberTest {
         Member member = Member.JoinOrganizer("aaaa", "adsf!@34", "adsf!@34", LocalDate.of(2022, 03, 03), "name", "남", "채식주의자");
 
         // when
-        member.enrollParticipant("aaaa","bbbb");
+        member.enrollParticipant("aaaa", "bbbb");
 
         // then
         assertThat(member.getMemberStatuses()).containsExactly(MemberStatus.ORGANIZER, MemberStatus.PARTICIPANT);
@@ -52,5 +53,29 @@ class MemberTest {
 
         // then
         assertThat(member.getOrganizerInfo().getTeam()).isEqualTo("채식주의자");
+    }
+
+    @Test
+    void 참가자는_참여자_정보를_수정할_수_있다() {
+        // given
+        Member member = Member.JoinParticipant("aaaa", "adsf!@34", "adsf!@34", LocalDate.of(2022, 03, 03), "name", "남", "adsf", "asdf");
+
+        // when
+        member.changeInfo("eeee", "eeee");
+
+        // then
+        assertAll(() -> {
+            assertThat(member.getParticipantInfo().getDietaryRestrictions()).isEqualTo("eeee");
+            assertThat(member.getParticipantInfo().getIntroduce()).isEqualTo("eeee");
+        });
+    }
+
+    @Test
+    void 주최자는_참여자_정보를_수정하면_예외를_일으킨다() {
+        // given
+        Member member = Member.JoinOrganizer("aaaa", "adsf!@34", "adsf!@34", LocalDate.of(2022, 03, 03), "name", "남", "채식주의자");
+
+        // then
+        assertThatIllegalArgumentException().isThrownBy(() -> member.changeInfo("eeee", "eeee"));
     }
 }
